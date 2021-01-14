b988b4e78db0cca32f2fd6bdbcb0d05b9b1a9d57
Harrison Malone <harrison.malone2@gmail.com>
2021-01-14 12:35:16 +1100

    added border bottom at the end of every year block

--- src/styles/Year.js
+++ src/styles/Year.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const AlbumContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  border-bottom: 1px solid black;
   @media only screen and (max-width: 915px) {
     justify-content: center;
   }
